198b08ad8bd334af7525e567c56029e03b63a33b
Ederlin Ortega <ederling91@gmail.com>
2026-02-21 16:32:29 -0400

    Actualizacion

--- react-fetch-content/src/App.js
+++ react-fetch-content/src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
+import Comentarios from "./Ederlin/Comentarios";
 import ListarUsuarios from "./Usuario/ListarUsuarios";
 
 function App() {
   return (
     <>
       <ListarUsuarios />
+      <Comentarios/>
     </>
   );
 }
---
+++ react-fetch-content/src/Ederlin/Comentarios.jsx
@@ -0,0 +1,32 @@
+import { useState, useEffect } from "react";
+import './Api.css'
+
+
+
+export default function Comentarios() {
+   
+     const [comentario, setComentario] = useState([]);
+
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/comments')
+      .then(response => response.json())
+      .then(data => setComentario(data));
+  }, []);
+
+  return (
+    <div className="fondo">
+      <h2 className="titulo">Comentarios</h2>
+
+      <div className="contenedor">
+        {comentario.map(comentario => (
+        <div className="card" key={comentario.id}>
+          <h2 className="email">{comentario.email}</h2>
+          <p>{comentario.body}</p>
+        </div>
+         
+        //   <li key={comentarios.id}>{comentarios.email}</li>
+        ))}
+      </div>
+    </div>
+    );
+}
